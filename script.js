79bf50be2a76cc329f8e34f7ede4902e2c16dac2
lashakvi <kvinikadzelasha12@gmail.com>
2025-03-13 19:24:31 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const countryName = document.querySelector('input').value.trim();
+    const countryInfoDiv = document.querySelector('#countryInfo');
+if (!countryName){
+    countryInfoDiv.innerHTML = "<p> enter country name.</p>";
+    return;
+}
+      fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Country not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const country = data[0];
+            countryInfoDiv.innerHTML = `
+                <h2>${country.name.common}</h2>
+                <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+                <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+                <p><strong>Region:</strong> ${country.region}</p>
+                <img src="${country.flags.svg}" alt="Flag of ${country.name.common}">
+            `;
+        })
+        .catch(error => {
+            countryInfoDiv.innerHTML = `<p style="color: red;">${error.message}</p>`;
+        });
+});
